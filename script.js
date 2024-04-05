243b11ebcb2dad8f31f1734b1f559c5883932396
sehajsb <sehajbrar1113@gmail.com>
2024-04-05 11:46:55 -0500

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	deleted:    labeled_images/Hunter/1.jpg
    	deleted:    labeled_images/Hunter/2.jpg
    	deleted:    labeled_images/Hunter/3.jpg
    	deleted:    labeled_images/Hunter/4.jpg
    	deleted:    labeled_images/Hunter/5.jpg
    	deleted:    labeled_images/Hunter/6.jpg
    	deleted:    labeled_images/Hunter/7.jpg
    	deleted:    labeled_images/Hunter/8.jpg
    	modified:   script.js

--- script.js
+++ script.js
@@ -172,7 +172,7 @@ async function start() {
 }
 
 function loadLabeledImages() {
-  const labels = ["Parker", "Sehaj", "Shubh", "Adam", "Hunter"];
+  const labels = ["Parker", "Sehaj", "Shubh", "Adam"];
   return Promise.all(
     labels.map(async (label) => {
       const descriptions = [];
